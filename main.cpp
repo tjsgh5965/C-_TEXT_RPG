0aa5bb6ab1e9e83116c961985a52df5007ee9534
QUOTIENT <tjsgh5965@naver.com>
2022-06-07 18:29:29 +0900

    test02
    moving

--- main.cpp
+++ main.cpp
@@ -1,10 +1,29 @@
 #include <iostream>
+#include <Windows.h>
 
 using namespace std;
 
+void gotoxy(int x,int y)
+{
+    COORD pos={x,y};
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos); 
+}
+
 int main() {
-    cout << "뭘봐 이자식아" << endl;
-    return 0;
-    // 끝
     
+    int x = 0, y = 0;
+
+    while (true) {
+        gotoxy(x, y);
+        cout << " ";
+        if (GetAsyncKeyState(VK_UP) & 0x8000) y--;
+        if (GetAsyncKeyState(VK_DOWN) & 0x8000) y++;
+        if (GetAsyncKeyState(VK_LEFT) & 0x8000) x--;
+        if (GetAsyncKeyState(VK_RIGHT) & 0x8000) x++;
+
+        gotoxy(x,y);
+        cout << "O";
+        Sleep(50);
+    }
+
 }
